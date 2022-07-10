e6eada0dae77060857dac38169eaab640c888812
Jakub Zawadzillo <zawadzillo.jakub@gmail.com>
2022-07-10 11:51:24 +0100

    Added WebDriverManager

--- src/main/java/com/framework/settings/webdriver/factory/WebDriverFactory.java
+++ src/main/java/com/framework/settings/webdriver/factory/WebDriverFactory.java
@@ -4,24 +4,5 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 
 public abstract class WebDriverFactory {
-    protected WebDriver driver;
-    protected abstract void setUpDriverOptions(boolean headless);
-    protected abstract void locateDriver();
-    protected abstract void tearDownDriver();
 
-    public WebDriver getDriver(boolean headless){
-        if (driver == null){
-            locateDriver();
-            setUpDriverOptions(headless);
-            return driver;
-        }
-        else throw new WebDriverException("Previous driver instance is still running");
-    }
-
-    public void killDriverInstance(){
-        if (driver != null){
-            tearDownDriver();
-        }
-        else throw new WebDriverException("Current Driver Instance is null");
-    }
 }
--- src/main/java/com/framework/settings/webdriver/manager/WebDriverManager.java
+++ src/main/java/com/framework/settings/webdriver/manager/WebDriverManager.java
@@ -1,4 +1,27 @@
 package com.framework.settings.webdriver.manager;
 
-public class WebDriverManager {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+
+public abstract class WebDriverManager {
+    protected WebDriver driver;
+    protected abstract void setUpDriverOptions(boolean headless);
+    protected abstract void locateDriver();
+    protected abstract void tearDownDriver();
+
+    public WebDriver getDriver(boolean headless){
+        if (driver == null){
+            locateDriver();
+            setUpDriverOptions(headless);
+            return driver;
+        }
+        else throw new WebDriverException("Previous driver instance is still running");
+    }
+
+    public void killDriverInstance(){
+        if (driver != null){
+            tearDownDriver();
+        }
+        else throw new WebDriverException("Current Driver Instance is null");
+    }
 }
